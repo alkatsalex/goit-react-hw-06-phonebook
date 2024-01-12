e59c52f8a2c61bfda3b9addce805a2d229b8b369
alkatsalex <alkatsalex@ukr.net>
2024-01-12 13:14:17 +0200

    fix filter

--- src/store/store.js
+++ src/store/store.js
@@ -19,3 +19,24 @@ const persistedReducer = persistReducer(persistConfig, reducer);
 export const store = configureStore({ reducer: persistedReducer });
 
 export const persistor = persistStore(store);
+
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import { ContactReducer } from './ContactSlice/ContactSlice';
+// import { FilterReducer } from './filterSlice/filterSlice';
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
+// const persistedReducer = persistReducer(persistConfig, ContactReducer);
+
+// const reducer = combineReducers({
+//   contacts: persistedReducer,
+//   filter: FilterReducer,
+// });
+
+// export const store = configureStore({ reducer });
+
+// export const persistor = persistStore(store);
